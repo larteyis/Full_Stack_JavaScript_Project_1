24123f3c3e71f9218d5172951e531c5fc5468ec0
larteyis <Lartey.isaac@gmail.com>
2021-09-30 22:17:40 -0400

    09.30.2021
    Quotes not displaying properly fully fixed

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+  //Creating a "quote" array and adding data to "quote" object
 const quotes =[
   {quote: "Keep smiling, because life is a beautiful thing and there's so much to smile about.", source: "Marilyn Monroe", citation: "Movie", year: "1958"},
   {quote: "Life is a long lesson in humility.", source: "James M. Barrie" },
@@ -18,46 +17,36 @@ const quotes =[
   {quote: "Life is trying things to see if they work.", source: "Ray Bradbury"}
 ];
 
-//console.log(quotes);
+//console.log(quotes); // Test data array
 
 
-
-
-/***
- * `getRandomQuote` function
-***/
+// Creating a "getRandomQuote" function
 function getRandomQuote (arr) {
-        var random = Math.floor(Math.random() * quotes.length);
-        for (var i = 0; i < arr.length; i++) {
-        var randomQuotes = arr[random];  
-  }
-        return randomQuotes;
+  var random = Math.floor(Math.random() * quotes.length); // generate random numbers based on the length of quotes
+  for (var i = 0; i < arr.length; i++) { 
+  var randomQuotes = arr[random];  
+} // loop randomQuotes
+  return randomQuotes; 
 }
-
-//console.log(getRandomQuote(quotes));
-
-/***
- * `printQuote` function
-***/
+//console.log(getRandomQuote(quotes)); //Test function 
 
 
+// Create a "printQuote" function
 function printQuote () {
-  let outputRandomQuote = (getRandomQuote(quotes));
-  let html = `<p class="quote"> ${outputRandomQuote.quote} </p>
-  <p class="source"> ${outputRandomQuote.source}; 
+  let outputRandomQuote = (getRandomQuote(quotes));//call "getRandomQuote" function
   
-  if ("citation" in quotes) {
-    html += <span class="citation"> ${outputRandomQuote.citation} </span>;
+  let html = `<p class="quote"> ${outputRandomQuote.quote} </p>
+  <p class="source"> ${outputRandomQuote.source}`; //html string to store variables
+  
+  if (outputRandomQuote["citation"]) {
+    html += `<span class="citation"> ${outputRandomQuote.citation} </span>`;//html string to check for "citation"
   };
-  if ("year" in quotes) { 
-    html += <span class="year"> ${outputRandomQuote.year} </span>;
+  if (outputRandomQuote["year"]) { 
+    html += `<span class="year"> ${outputRandomQuote.year} </span>`;//html string to check for "year"
     } 
-    </p>`;
+    `</p>`;
     document.getElementById("quote-box").innerHTML = html;
 }
-
-printQuote();
-
 
 /***
  * click event listener for the print quote button
